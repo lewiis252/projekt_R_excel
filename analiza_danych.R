e1a9b534f06dd15323d2056b54faf2a68e2c9749
lewiis252 <krystianoleniacz@gmail.com>
2023-01-21 20:22:40 +0100

    obrazki

--- analiza_danych.R
+++ analiza_danych.R
@@ -4,14 +4,12 @@ visa <- read.csv("VISA.csv", header = TRUE, dec = ".")
 mastercard <- read.csv("MASTERCARD.csv", header = TRUE, dec = ".")
 
 # funkcja do obliczenia średniej kroczącej
-rolling_average <- function(x, n = 7){
+rolling_average <- function(x, n = 5){
     srednia_kroczaca <- filter(x, rep(1 / n, n))
     srednia_kroczaca <- srednia_kroczaca[-(1:3)]
     srednia_kroczaca <- srednia_kroczaca[- ((length(srednia_kroczaca)-2): length(srednia_kroczaca))]
     return(srednia_kroczaca)
     }
-
-
 
 visa$cena_srednia <- (visa$High + visa$Low) / 2
 mastercard$cena_srednia <- (mastercard$High + mastercard$Low) / 2
@@ -22,7 +20,7 @@ srednia_kroczaca_mastercard <- rolling_average(mastercard$cena_srednia)
 # wykres cen średniej kroczącej obu spółek
 pdf(file = "wykres.pdf")
 plot(srednia_kroczaca_visa, type = "l", col = "blue", xlab = "Data", ylab = "Cena", ylim = c(140, 400), lty = 1, xaxt = "n", ,main='Ceny akcji w latach 2019-2022')
-lines(srednia_kroczaca_mastercard, type = "l", col = "orange", lty = 1)
+lines(srednia_kroczaca_mastercard, type = "l", col = "red", lty = 1)
 axis(1,
     at = c(0, 200, 400, 600, 800, 1000),
     labels = c(
@@ -33,7 +31,7 @@ axis(1,
 
 legend(100, 400,
     legend = c("Visa", "Mastercard"),
-    col = c("blue", "orange"), lty = 1, cex = 0.8
+    col = c("blue", "red"), lty = 1, cex = 0.8
 )
 dev.off()
 
@@ -58,8 +56,13 @@ cor(mastercard$Rozstep,mastercard$Volume)
 
 cor(visa$Volume, mastercard$Volume)
 
-sum(visa$Volume)
-sum(mastercard$Volume)
+
+# współczynniki zmienności 
+sd(visa$cena_srednia)/mean(visa$cena_srednia)
+sd(mastercard$cena_srednia)/mean(mastercard$cena_srednia)
+
+mean((mastercard$cena_srednia)/(visa$cena_srednia))
+ 
 
 pdf(file = "histogram.pdf")
 par(mfrow = c(1, 2))
@@ -77,32 +80,38 @@ dev.off()
 (zysk_mastercard <- (tail(mastercard$cena_srednia, 1) - mastercard$cena_srednia[1]) / mastercard$cena_srednia[1])
 
 
-stopa_zwrotu_zakup_akcji_co_x_dni <- function(dataframe_name, days) {
-    cena_srednia <- (dataframe_name$High + dataframe_name$Low) / 2
+# stopa_zwrotu_zakup_akcji_co_x_dni <- function(dataframe_name, days) {
+#     cena_srednia <- (dataframe_name$High + dataframe_name$Low) / 2
 
-    dzien_zakupu <- rep_len(c(1, rep(0, (days-1))), length.out = nrow(dataframe_name)) # make column that storage day of purchase
+#     dzien_zakupu <- rep_len(c(1, rep(0, (days-1))), length.out = nrow(dataframe_name)) # make column that storage day of purchase
 
-    liczba_akcji <- 100 / cena_srednia * dzien_zakupu
+#     liczba_akcji <- 100 / cena_srednia * dzien_zakupu
 
-    skumulowana_liczba_akcji <- cumsum(liczba_akcji)
+#     skumulowana_liczba_akcji <- cumsum(liczba_akcji)
 
-    wartosc_portfela <- skumulowana_liczba_akcji * cena_srednia
+#     wartosc_portfela <- skumulowana_liczba_akcji * cena_srednia
 
-    zainwestowane_srodki <- cumsum(dzien_zakupu * 100)
+#     zainwestowane_srodki <- cumsum(dzien_zakupu * 100)
 
-    n <- nrow(dataframe_name)
+#     n <- nrow(dataframe_name)
 
-    stopa_zwrotu <- (wartosc_portfela[n] - zainwestowane_srodki[n]) / zainwestowane_srodki[n]
-    stopa_zwrotu
-}
+#     stopa_zwrotu <- (wartosc_portfela[n] - zainwestowane_srodki[n]) / zainwestowane_srodki[n]
+#     stopa_zwrotu
+# }
 
-stopa_zwrotu_zakup_akcji_co_x_dni(visa,30)
-stopa_zwrotu_zakup_akcji_co_x_dni(mastercard,30)
+# stopa_zwrotu_zakup_akcji_co_x_dni(visa,30)
+# stopa_zwrotu_zakup_akcji_co_x_dni(mastercard,30)
 
 # agregacja 
+
+# liczba obróconych akcji w roku
 visa$year <- format(as.Date(visa$Date, format="%Y-%m-%d"),"%Y")
 mastercard$year <- format(as.Date(visa$Date, format="%Y-%m-%d"),"%Y")
-visa$year
-?aggregate
+
+sum(visa$Volume)
+sum(mastercard$Volume)
+
 aggregate(visa$Volume, by=list(visa$year), FUN=sum)
 aggregate(mastercard$Volume, by=list(visa$year), FUN=sum)
+
+
